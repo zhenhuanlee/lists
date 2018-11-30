03ab68bb939048f5181e3138abdbad1d63108661
zhenhuanlee <zhenhuanlee@outlook.com>
2018-11-30 15:58:26 +0800

    third drop

--- src/lib/third.rs
+++ src/lib/third.rs
@@ -62,6 +62,19 @@ impl<T> List<T> {
     }
 }
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
